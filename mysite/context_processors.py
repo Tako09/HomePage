df06955b7252f4cfb77fbe4c65a1f26232df16c9
Tako09 <ikedat350@gmail.com>
2021-06-06 23:32:50 +0900

    Blog App

---
+++ mysite/context_processors.py
@@ -0,0 +1,15 @@
+# レンタリング(使うと明示してないクラス)の変数の
+# 使用を許可するためのプログラム
+
+from django.db.models import Count, Q 
+from blog.models import Category, Tag
+
+def common(request):
+    context = {
+        'categories': Category.objects.annotate(
+            num_posts=Count('post', filter=Q(post__is_public=True))
+        ),
+        'tags': Tag.objects.annotate(
+            num_posts=Count('post', filter=Q(post__is_public=True)))
+    }
+    return context
